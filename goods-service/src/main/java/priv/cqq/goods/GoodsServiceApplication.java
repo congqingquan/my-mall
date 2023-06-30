f9acc6c17ba60ad1c898e8d0282ff28183d0dbcd
congqingquan <17600073620@163.com>
2023-06-30 17:38:05 +0800

    Some code

--- goods-service/src/main/java/priv/cqq/goods/GoodsServiceApplication.java
+++ goods-service/src/main/java/priv/cqq/goods/GoodsServiceApplication.java
@@ -1,13 +1,40 @@
 package priv.cqq.goods;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GoodsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GoodsServiceApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+
+    @RestController
+    public class GoodsController {
+
+        @Autowired
+        private RestTemplate restTemplate;
+
+        @GetMapping(value = "/callOrder/{string}")
+        public String echo(@PathVariable String string) {
+            return restTemplate.getForObject("http://nacos-order-service/echo/" + string, String.class);
+        }
+    }
 }
--- order-service/src/main/java/priv/cqq/order/OrderServiceApplication.java
+++ order-service/src/main/java/priv/cqq/order/OrderServiceApplication.java
@@ -1,13 +1,30 @@
 package priv.cqq.order;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    @RestController
+    public class OrderController {
+
+        @Value("${server.port}")
+        private String port;
+
+        @GetMapping(value = "/echo/{string}")
+        public String echo(@PathVariable String string) {
+            return String.format("Nacos Discovery %s %s", port, string);
+        }
+    }
 }
