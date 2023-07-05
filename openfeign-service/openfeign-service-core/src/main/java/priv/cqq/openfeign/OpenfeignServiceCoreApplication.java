391aec27183a2aaeb1b7a222070e550ec6e91150
congqingquan <17600073620@163.com>
2023-07-05 17:31:13 +0800

    OpenFeign service

---
+++ openfeign-service/openfeign-service-core/src/main/java/priv/cqq/openfeign/OpenfeignServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package priv.cqq.openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignServiceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenfeignServiceCoreApplication.class, args);
+    }
+
+}
---
+++ openfeign-service/openfeign-service-core/src/test/java/priv/cqq/openfeign/OpenfeignServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.cqq.openfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeignServiceCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
