c0b33591a72ad5f1984071decd528ad69a6e1a1b
congqingquan <congqingquan@bmsmart.com.cn>
2023-07-20 00:31:43 +0800

    Some code

---
+++ sentinel-service/src/main/java/priv/cqq/sentinel/config/FeignConfig.java
@@ -0,0 +1,39 @@
+package priv.cqq.sentinel.config;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import feign.Logger;
+import feign.Request;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration // @Configuration 表示为全局 Feign 配置
+public class FeignConfig {
+
+    // 日志配置
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.HEADERS;
+    }
+
+
+    // 超时配置
+    @Bean
+    public Request.Options orderFeignRequestOptions() {
+        // 1. connectTimeout: 建立连接超时时长
+        // 2. readTimeout: 建立连接超时后最多等待被调用方响应时长
+        // 3. followRedirects: 若响应 3xx 是否进行重定向
+        return new Request.Options(
+                5, TimeUnit.SECONDS,
+                5, TimeUnit.SECONDS, false
+        );
+    }
+
+    // 负载均衡配置
+    @Bean
+    public IRule feignLoadbalancer() {
+        return new RoundRobinRule();
+    }
+}
--- sentinel-service/src/main/java/priv/cqq/sentinel/degrade/OpenfeignController.java
+++ sentinel-service/src/main/java/priv/cqq/sentinel/degrade/OpenfeignController.java
@@ -1,26 +1,78 @@
 package priv.cqq.sentinel.degrade;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class OpenfeignController {
-
-    @FeignClient(name = "nacos-sentinel-service")
+    
+    @FeignClient(name = "nacos-sentinel-service", fallbackFactory = FeignService.FeignFallbackFactory.class)
     public interface FeignService {
-
-        @GetMapping(value = "/echo/{string}")
+        
+        @GetMapping(value = "/sentinel/feign/echo/{string}")
         String echo(@PathVariable String string);
+        
+        @Slf4j
+        @RestController
+        class FeignController implements FeignService {
+            
+            @SentinelResource(value = "echo", blockHandler = "handleEchoException")
+            @Override
+            public String echo(String string) {
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return string;
+            }
+            
+            public static String handleEchoException(String string, BlockException blockException) {
+                log.error(blockException.getMessage());
+                return "handleEchoException";
+            }
+        }
+        
+        @Slf4j
+        @Component
+        class FeignFallbackHandler implements FeignService {
+            
+            @Override
+            public String echo(String string) {
+                log.error("sentinel 熔断降级");
+                return "fake value";
+            }
+        }
+        
+        @Slf4j
+        @Component
+        class FeignFallbackFactory implements FallbackFactory<FeignFallbackHandler> {
+            
+            @Resource
+            private FeignFallbackHandler feignFallbackHandler;
+            
+            @Override
+            public FeignFallbackHandler create(Throwable cause) {
+                log.error(cause.getMessage());
+                return feignFallbackHandler;
+            }
+        }
     }
-
+    
     @Resource
     private FeignService feignService;
-
-    @GetMapping(value = "/sentinel/openfeign/{string}")
+    
+    @GetMapping(value = "/sentinel/openfeign/echo/{string}")
     public String echo(@PathVariable String string) {
         return feignService.echo(string);
     }
