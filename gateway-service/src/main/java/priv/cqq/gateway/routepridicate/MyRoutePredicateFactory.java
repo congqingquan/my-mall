936d391af02d78691ce3db2c3eb8072ec5803c95
congqingquan <17600073620@163.com>
2023-07-28 17:29:08 +0800

    Some code

---
+++ gateway-service/src/main/java/priv/cqq/gateway/routepridicate/MyRoutePredicateFactory.java
@@ -0,0 +1,12 @@
+package priv.cqq.gateway.routepridicate;
+
+/**
+ * Created by QQ.Cong on 2023-07-28 / 14:48
+ *
+ * @Description
+ */
+// 1. 必须是一个 Spring Bean
+// 2. 必须以 RoutePredicateFactory 作为类名后缀
+// 3. 继承 AbstractRoutePredicateFactory
+//public class MyRoutePredicateFactory extends AbstractRoutePredicateFactory<CheckA> {
+//}
