b5dfb260591e2a014af07528948f977c7d8a4436
congqingquan <congqingquan@bmsmart.com.cn>
2023-08-05 22:30:27 +0800

    Seata service

---
+++ seata-service/src/main/java/priv/cqq/seata/SeataServiceApplication.java
@@ -0,0 +1,13 @@
+package priv.cqq.seata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SeataServiceApplication.class, args);
+    }
+    
+}
---
+++ seata-service/src/test/java/priv/cqq/seata/SeataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.cqq.seata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeataServiceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
