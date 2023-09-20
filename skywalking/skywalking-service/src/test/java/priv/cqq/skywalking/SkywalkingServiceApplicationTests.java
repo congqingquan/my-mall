53562114b20fddbf0e522d0dab395640a5b9c57f
congqingquan <17600073620@163.com>
2023-09-20 14:32:32 +0800

    skywalking

---
+++ skywalking/skywalking-service/src/test/java/priv/cqq/skywalking/SkywalkingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.cqq.skywalking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkywalkingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
